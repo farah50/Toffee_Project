85f7351e293ad961d73876e15f521b87c50d0285
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2023-05-08 15:37:09 +0300

    Add files via upload

--- Payment_Method.java
+++ Payment_Method.java
@@ -1,16 +1,19 @@
 import java.util.*;
 //This class is for choosing a payment way, either cash or through smart wallets.
-public class Payment_Method {
+public class Payment_Method extends Set_Values{
     public Payment_Method(){}
     public Scanner in = new Scanner(System.in);
 
-    public boolean payViaSmrtWallet(String SW){
+    public boolean payViaSmrtWallet(){
         System.out.println("Please enter your card number");
-        SW = in.nextLine();
+        String SW = in.nextLine();
         return true;
     }
 
     public void cashOnDelivry(){
-        //Call
+        setAddress();
+        getAddress();
+        setPhoneNumber();
+        getPhoneNumber()
     }
 }

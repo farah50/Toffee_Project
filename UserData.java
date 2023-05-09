26f7c212c9421428f163f51feffd665d75e6ac21
Farah Mohamed <101725658+farah50@users.noreply.github.com>
2023-05-10 01:51:44 +0300

    Update UserData.java

--- UserData.java
+++ UserData.java
@@ -1,24 +1,15 @@
 abstract class UserData {
     private
 
-    String custName;
     String email;
     String password;
 
     public
 
-    UserData(String n, String e, String pw) {
+    UserData( String e, String pw) {
     }
 
     abstract void saveUserData();
-
-    void setCustName(String n) {
-        custName = n;
-    }
-
-    String getCustName() {
-        return custName;
-    }
 
     void setEmail(String e) {
         email = e;

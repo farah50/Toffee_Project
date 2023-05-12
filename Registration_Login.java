e203005caba5bf5308ded49e8beb11e4cdb58311
Farah Mohamed <101725658+farah50@users.noreply.github.com>
2023-05-12 14:31:09 +0300

    added validEmail method & searchForEmail method

--- Registration_Login.java
+++ Registration_Login.java
@@ -1,55 +1,160 @@
 import java.util.Scanner;
+import java.util.regex.*;
+import java.io.File;
+import java.io.FileWriter;
 
 class Registration extends UserData{
     private  
+        String name; 
         String phone;
         String address; 
 
     public 
         //constructor
-        Registration( String n, String e, String pw, String a, String p){        
-            suber(n, e, pw);
+        Registration( String e, String pw, String n, String a, String p){        
+            suber(e, pw);
+            name = n
             Address = a;
             Phone = p;         
         }
     
-         // default constructor
+        // default constructor
         Registration() {
-            super("", "", "");
+            super("", "");
+            name = "";
             phone = "";
             address = "";
+        }
+    
+        // Setter & getter
+        void setName(String n) {
+            name = n;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        void setAddress(String a) {
+            address = a;
+        }
+
+        String getAddress() {
+            return address;
+        }
+
+        void setPhone(String p) {
+            phone = p;
+        }
+
+        String getPhone() {
+            return phone;
         }
 
 
         void register(){
               try (Scanner input = new Scanner(System.in)) {
-                System.out.println("Plaese enter your personal data.");
-                System.out.print("Name: ");
-                setCustName(input.nextLine());
+                   System.out.println("Plaese enter your personal data.");
+                   System.out.print("Name: ");
+                   setCustName(input.nextLine());
 
-                System.out.print("Phone: ");
-                setPhone(input.nextLine());
+                   System.out.print("Phone: ");
+                   setPhone(input.nextLine());
 
-                System.out.print("Address: ");
-                setAddress(input.nextLine());
-            }
+                   System.out.print("Address: ");
+                   setAddress(input.nextLine());
+                  
+                   boolean validEmail;
+                   boolean emailExists;
+                  
+                   do {
+                        System.out.print("E-mail: ");
+                        setEmail(input.nextLine());
+
+                        // check if the email address is valid
+                        validEmail = validEmail();
+
+                        // check if the email address exists in the file
+                        emailExists = searchForEmail(getEmail());
+
+                        if (!validEmail) {
+                             System.out.println("Invalid email, please try again.");
+                        }
+                        else if (emailExists) {
+                             System.out.println("This email already exists, please enter another email.");
+                        }
+                    } while (!validEmail || emailExists);
+                     
+                    System.out.print("Password: ");
+                    setPassword(input.nextLine());
+
+                    // save user data to file
+                    saveUserData();
+               }       
         }
+    
+       // method to validate email address
+       public boolean validEmail() {
+            String pattern = "(\\w+)(\\.\\w+)*@(\\w+\\.)(com|edu|org|net|co)";
+            Pattern regex = Pattern.compile(pattern);       // Pattern class fetches and returns the regular expression in the  string format                                           
+            Matcher regexMatch = regex.matcher(getEmail()); // Matcher class is used to match the input sequence against the whole text                       
+            return regexMatch.matches();
+       }
+    
+    
+        // method to search for an email address in the file
+        public boolean searchForEmail(String email) {
+            try {
+                Scanner scanner = new Scanner(new File("information.txt"));
+                while (scanner.hasNextLine()) {            // hasNextLine() method Returns true if there is another line in the input of this scanner.
+                    String[] fields = scanner.nextLine().split("\\|");
+                    if (fields.length >= 4 && fields[3].trim().equals(email)) {
+                        scanner.close();
+                        return true;                      // email found in the file
+                    }
+                }
+                scanner.close();
+            } 
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;                                // email not found in the file
+        }
+
         
+        void valid_phone() {
+             // will be updated
+        }
+    
         void enter_strong_password(){
             // will be updated
         }
-
-        String hide_Pw(){
-            // will be updated
-        }
-
+    
         void XOR_encrypt_pw(){
             // will be updated
         }
+    
+    
+        @Override
+        public void saveUserData() {
+            try (Scanner input = new Scanner(System.in)) {
+                try {
+                    FileWriter writer = new FileWriter("information.txt", true);
+                    writer.write(getName() + "|" + getPhone() + "|" + getAddress() + "|" + getEmail() + "|" + getPassword()
+                            + "\n"); // write method used to write the String on the file
+                    writer.close();
+                    System.out.println("registered successfully ^_^");
+                }
 
-        void valid_email(String& Email){
-           // will be updated
-        }
+                catch (Exception e) { // catch allows u to define a block of code to be executed, if an error occurs
+                                      // in the try block.
+                    e.printStackTrace(); // tool used to handle exceptions and errors
+                }
+
+                input.close();
+            }
+        }  
+    
 
 }
 

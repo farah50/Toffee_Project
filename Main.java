d69bb6c44595db6dc48a9a629f47c96e9bc11440
Farah Mohamed <101725658+farah50@users.noreply.github.com>
2023-05-12 22:53:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        Registration r1 = new Registration();
-        Login L1 = new Login();
         
+        Scanner input = new Scanner(System.in);
         String choice;
-        System.out.println("----------------welcom-----------------");
-        System.out.println("choose what you want from the menue");
-        System.out.println("1- Registration");
-        System.out.println("2- Login");
-        System.out.println("3- View Catalog");
-        System.out.println("4- Creat Order");
-        System.out.println("5- Exit");
+
         
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("My choice: ");
+        while (true) {
+            System.out.println("----------------welcom-----------------");
+            System.out.println("choose what you want from the menue");
+            System.out.println("1- Registration");
+            System.out.println("2- Login");
+            System.out.println("3- View Catalog");
+            System.out.println("4- Creat Order");
+            System.out.println("5- Exit");
+            
+            System.out.println("My choice: ");
             choice = input.nextLine();
-        }
-        if (choice == "1") {
-            r1.register();
-        } 
-        else if (choice == "2") {
-            L1.login();
-        } 
-        else if (choice == "3") {
-            // view catalog
-        }
-        else if (choice == "4") {
-            // creat order
-        } 
-        else if (choice == "5") {
-            // Exit
+            
+            switch (choice) {
+                case "1":
+                    Registration r1 = new Registration();
+                    r1.register();
+                    break;
+                case "2":
+                    Login L1 = new Login();
+                    L1.login();
+                    break;
+                case "3":
+                    System.out.println("Exiting program...");
+                    input.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+                    break;
+            }
         }
     }
-    
+} 

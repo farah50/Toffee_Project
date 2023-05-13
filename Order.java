48f90cada63cf9abf1a53ec4cd4c6346dcdaae24
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2023-05-13 20:16:35 +0300

    Add files via upload

--- Order.java
+++ Order.java
@@ -1,54 +1,62 @@
+import java.io.IOException;
 import java.util.*;
 
 public class Order {
-    private String ID;
+    private int ID;
     private String Address;
     private String Phone_Number;
     private int Loyalty_points;
-    private int Total_Cost;
-
+    private float Total_Cost = 0;
     public Scanner in = new Scanner(System.in);
+    public Catalog c = new Catalog();
+
+    //public Scanner in = new Scanner(System.in);
     public Order(){}
 
     //This function is to creatiing an order.
-    public void createOrder(){
+    public void createOrder() throws IOException {
         System.out.println("Order creation, please choose your items");
+        c.viewCatalog();
+        addItemsToShoppingCart();
+        System.out.println("Your total cost is = " + getTotalCost());
+    }
+    public void addItemsToShoppingCart(){
+        System.out.println("How many items do you want to buy ");
+        int items;
+        items = in.nextInt();
+        System.out.println("Please insert their IDS");
+        int ShoppingCart[] = new int[items];
+        for (int i = 0 ; i<items ; i++){
+            if(items <= 50){
+                ID = in.nextInt();
+                ShoppingCart[i] = ID;
+                if (ID == 1){
+                    Total_Cost += 250;
+                } else if (ID == 2) {
+                    Total_Cost += 300;
+                } else if (ID == 3) {
+                    Total_Cost += 250;
+                } else if (ID == 4) {
+                    Total_Cost += 500;
+                }else System.out.println("Invalid ID");
+
+            }
+        }
+        for (int i = 0 ; i<items ; i++){
+            if (ShoppingCart[i] == 1){
+                System.out.println("ID is: " + ShoppingCart[i] + " Chocolate Cake");
+            } else if (ShoppingCart[i] == 2) {
+                System.out.println("ID is: " + ShoppingCart[i] + " Red Velvet Cake");
+            } else if (ShoppingCart[i] == 3) {
+                System.out.println("ID is: " + ShoppingCart[i] + " Strawberry Cake");
+            } else if (ShoppingCart[i] == 4) {
+                System.out.println("ID is: " + ShoppingCart[i] + " Galaxy Cake");
+            }else System.out.println("Invalid ID");
+        }
     }
 
-    //This function is to cancelling an order.
-    public void cancelOrder(){
-        System.out.println("Are you sure that you want to canel this order?");
-        System.out.println("Please type (Yes or no)");
-        String choice = in.next();
-
-        if(choice.equals("yes"))
-            System.out.println("Order cancelled");
-        //order will be deleted from database.
+    public float getTotalCost() {
+        return Total_Cost;
     }
-
-    //This function to reorder a previous order
-    public void reOrder(){
-        System.out.println("Please choose which order you want to order again?");
-        //User should choose from order history
-    }
-
-
-    public void setLoyaltyPoint(){
-        System.out.println("Please enter number points that you want to add to this user?");
-        int lp = in.nextInt();
-        this.Loyalty_points = lp;
-    }
-
-    public int getLoyaltyPoint (){
-        return Loyalty_points;
-    }
-
-    /*public int confirmOrder (int p , String v){
-
-    }
-
-    public int calcTotalCost(){
-
-    }*/
 }
 

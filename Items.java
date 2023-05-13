1b45b3b5c7b2d1db139e58e89eec0d656714e0bf
Farah Mohamed <101725658+farah50@users.noreply.github.com>
2023-05-13 18:17:49 +0300

    Update Items.java

--- Items.java
+++ Items.java
@@ -5,14 +5,7 @@ public class Items {
         try {
             FileWriter fr = new FileWriter("Catalog.txt");
             PrintWriter pr = new PrintWriter(fr , true);
-            pr.println("ID:     " + "Item:              " + "Price: ");
-            pr.println("1       " + "Chocolate Cake     " + "250.00 L.E. ");
-            pr.println("2       " + "Red velvet Cake    " + "300.00 L.E. ");
-            pr.println("3       " + "Strawberry Cake    " + "200.00 L.E. ");
-            pr.println("4       " + "Galaxy Cake        " + "500.00 L.E. ");
-            System.out.println("Built Successfully");
-            pr.close();
-
+          
         } catch (IOException e) {
             throw new RuntimeException(e);
         }

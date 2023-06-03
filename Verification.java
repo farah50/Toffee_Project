fe45948e0814460eef8679ee4f97e6e7018c7742
Farah Mohamed <101725658+farah50@users.noreply.github.com>
2023-06-03 10:45:57 +0300

    Update Verification.java

--- Verification.java
+++ Verification.java
@@ -1,25 +1,45 @@
+
 import java.util.*;
+import java.util.Scanner;
 
 public class Verification {
 
-    char[] OTP(String email, int len) {
-        System.out.print("You OTP is : ");
+    Scanner input = new Scanner(System.in);
 
-        long seed = email.hashCode();
+    String OTP(String email, int len) {
+
+        int emailHashCode = email.hashCode(); // hashCode() is a built-in method, generate a unique integer value
 
         // Using numeric values
         String numbers = "0123456789";
 
         // Using random method
-        Random rndm_method = new Random(seed);
+        Random random = new Random(emailHashCode);
 
         char[] otp = new char[len];
 
         for (int i = 0; i < len; i++) {
 
-            otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
+            otp[i] = numbers.charAt(random.nextInt(numbers.length()));
         }
-        return otp;
+        return new String(otp);
+
+    }
+
+    String sendOtpToEmail(String email) {
+        int length = 6;
+        System.out.print("email: ");
+        System.out.println(email);
+        String OTP = OTP(email, length);
+        SendMail m1 = new SendMail(email, OTP);
+        m1.send();
+        return OTP;
+    }
+
+    boolean Valid_OTP(String s) {
+        System.out.print("Enter OTP: ");
+        String userOTP = input.nextLine();
+        return userOTP.equals(s);
     }
 
 }
